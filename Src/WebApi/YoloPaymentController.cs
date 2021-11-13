8b8770ff8f5cf3163529b8970a0c6632abf88075
safestak-keith <keith@safestak.com>
2021-11-13 15:21:29 +0100

    Addressed warnings around nullables and cleaned up some tests

---
+++ Src/WebApi/YoloPaymentController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mintsafe.Lib;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mintsafe.WebApi
+{
+    public class YoloPayment
+    {
+        public string SourceAddress { get; set; }
+        public string DestinationAddress { get; set; }
+        public string[] Message { get; set; }
+        public string SigningKeyCborHex { get; set; }
+    }
+    
+    [ApiController]
+    [Route("[controller]")]
+    public class YoloPaymentController : ControllerBase
+    {
+        private readonly ILogger<YoloPaymentController> _logger;
+        private readonly IYoloWalletService _walletService;
+
+        public YoloPaymentController(
+            ILogger<YoloPaymentController> logger,
+            IYoloWalletService walletService)
+        {
+            _logger = logger;
+            _walletService = walletService;
+        }
+
+        [HttpPost("{address}")]
+        public async Task<string> Post(YoloPayment yoloPayment, CancellationToken ct)
+        {
+            await Task.Delay(50, ct);
+
+            var txHash = "";
+
+            return txHash;
+        }
+    }
+}
