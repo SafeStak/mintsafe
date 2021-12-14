fbaa08b421a1777f6cb076580aaa442c43ebc8df
safestak-keith <keith@safestak.com>
2021-12-14 15:47:57 +0000

    Temporary removal of tests for allocation

--- Tests/Lib.UnitTests/NiftyAllocatorShould.cs
+++ Tests/Lib.UnitTests/NiftyAllocatorShould.cs
@@ -24,38 +24,39 @@ public class NiftyAllocatorShould
             _mockSaleContextStore.Object);
     }
 
-    [Theory]
-    [InlineData(1, 0, 1, 1, 1)]
-    [InlineData(500, 0, 2, 500, 2)]
-    [InlineData(127, 373, 2, 500, 2)]
-    [InlineData(5, 495, 5, 500, 5)]
-    [InlineData(10000, 0, 10, 1000, 10)]
-    [InlineData(10000, 900, 10, 1000, 10)]
-    [InlineData(9800, 200, 100, 10000, 100)]
-    public async Task Allocate_Tokens_Correctly_Given_Spare_Mintable_Tokens_And_Sale_Allocations_When_Purchase_Request_Requested_Quantity_Is_Within_Limits(
-        int saleMintableCount,
-        int saleAllocatedCount,
-        int requestedQuantity,
-        int saleReleaseQuantity,
-        int expectedAllocatedQuantity)
-    {
-        var sale = GenerateSale(totalReleaseQuantity: saleReleaseQuantity);
-        var collection = GenerateCollection();
-        var mintableTokens = GenerateTokens(saleMintableCount);
-        var allocatedTokens = GenerateTokens(saleAllocatedCount);
-        var saleContext = GenerateSaleContext(sale, collection, mintableTokens, allocatedTokens);
-        var request = new PurchaseAttempt(
-            Guid.NewGuid(),
-            Guid.NewGuid(),
-            new Utxo("", 0, new[] { new Value(Assets.LovelaceUnit, 1000000) }),
-            requestedQuantity,
-            0);
+    // TODO: Move to SaleAllocationFileStore tests
+    //[Theory]
+    //[InlineData(1, 0, 1, 1, 1)]
+    //[InlineData(500, 0, 2, 500, 2)]
+    //[InlineData(127, 373, 2, 500, 2)]
+    //[InlineData(5, 495, 5, 500, 5)]
+    //[InlineData(10000, 0, 10, 1000, 10)]
+    //[InlineData(10000, 900, 10, 1000, 10)]
+    //[InlineData(9800, 200, 100, 10000, 100)]
+    //public async Task Allocate_Tokens_Correctly_Given_Spare_Mintable_Tokens_And_Sale_Allocations_When_Purchase_Request_Requested_Quantity_Is_Within_Limits(
+    //    int saleMintableCount,
+    //    int saleAllocatedCount,
+    //    int requestedQuantity,
+    //    int saleReleaseQuantity,
+    //    int expectedAllocatedQuantity)
+    //{
+    //    var sale = GenerateSale(totalReleaseQuantity: saleReleaseQuantity);
+    //    var collection = GenerateCollection();
+    //    var mintableTokens = GenerateTokens(saleMintableCount);
+    //    var allocatedTokens = GenerateTokens(saleAllocatedCount);
+    //    var saleContext = GenerateSaleContext(sale, collection, mintableTokens, allocatedTokens);
+    //    var request = new PurchaseAttempt(
+    //        Guid.NewGuid(),
+    //        Guid.NewGuid(),
+    //        new Utxo("", 0, new[] { new Value(Assets.LovelaceUnit, 1000000) }),
+    //        requestedQuantity,
+    //        0);
 
-        var allocated = await _allocator.AllocateNiftiesForPurchaseAsync(
-            request, saleContext);
+    //    var allocated = await _allocator.AllocateNiftiesForPurchaseAsync(
+    //        request, saleContext);
 
-        allocated.Length.Should().Be(expectedAllocatedQuantity);
-    }
+    //    allocated.Length.Should().Be(expectedAllocatedQuantity);
+    //}
 
     [Theory]
     [InlineData(1, 1, 1, 1)]
